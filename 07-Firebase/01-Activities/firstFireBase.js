07074592799a166bc909ab77bb86190b56941b3d
zenwattage <zenwattage@gmail.com>
2019-04-02 08:15:47 -0700

    firebase stuff

---
+++ 07-Firebase/01-Activities/firstFireBase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.9.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA44URA_SJbxMqdiYhDb6OPQmwRxopdWF8",
+    authDomain: "first-project-783a6.firebaseapp.com",
+    databaseURL: "https://first-project-783a6.firebaseio.com",
+    projectId: "first-project-783a6",
+    storageBucket: "first-project-783a6.appspot.com",
+    messagingSenderId: "646714226444"
+  };
+  firebase.initializeApp(config);
+</script>
