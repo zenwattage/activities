08bb4e59d40c91ab13a89e8cb523561bfa2a4c58
Scott H <zenwattage@gmail.com>
2019-06-22 15:59:02 -0700

    pushing from desk

--- trycatchtest.js
+++ trycatchtest.js
@@ -1,16 +1,32 @@
 try {
 
-    
+    var emptyArray = [];
 
-    var array = [0,1,2,3,4];
+    var array = [0,2,2,4,3,2,8,4];
     
+function minNum(array) {  
+    var min = Math.min(...array);
 
-    if(array.length >= 5) {
-        console.log("yep");
+    for(var i = 0; i < array.length; i++){
+
+        console.log("index is: " + i + " Num is: " + array[i]);
+        //if num === num
+        // get
+        // i of lower num
+        for(var j = i + 1; j < array.length; j++){
+            //console.log(j);
+
+            if(array[i] === array[j]) {
+                console.log()
+                console.log("This one matches: " +"i=" + i +"j="+ j);
+            }
+        }
     }
+    //console.log(min);
 
+}
 
-    
+minNum(emptyArray);
 
 } catch (error) {
     console.log(error);
